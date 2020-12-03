c4483c71daa0893ef0b96096bb69d134418e2ac5
MinhKhoi <khoidm@vnext.com.vn>
2020-12-03 13:08:01 +0700

    style cho side bar chua lam xong calendar

---
+++ vnext-abitdev/src/components/main/sidebar/style.js
@@ -0,0 +1,34 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+	root: {
+		width: "100%",
+		height: '100%',
+		maxWidth: 260,
+		backgroundColor: theme.palette.background.paper,
+	},
+	nested: {
+		paddingLeft: theme.spacing(4),
+	},
+	listsidebar: {
+		overflow: "scroll",
+		height: '39rem'
+	},
+	textPrimary: {
+		"& > span": {
+			fontSize: "15px!important",
+			color: "black",
+		},
+	},
+	textSubPrimary: {
+		"& > span": {
+			fontSize: "14px!important",
+		},
+	},
+	textSubPrimaryInfo: {
+		"& > span": {
+			fontSize: "14px!important",
+			width: "159px!important",
+		},
+	},
+}));
