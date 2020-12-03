42c1821f74f4aefd346447b3d673280b78b6e8c6
MinhKhoi <khoidm@vnext.com.vn>
2020-12-03 09:02:38 +0700

    done Appbar, add Main

--- vnext-abitdev/src/App.js
+++ vnext-abitdev/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Navbar from './components/Appbar/Appbar.js'
+import Main from './components/main/index.js'
 function App() {
   return (
     <div className="App">
-     
+     <Navbar/>
+     <Main/>
     </div>
   );
 }
